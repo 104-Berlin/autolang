2bbcb23b4d5aeb4dc8736faa9571758750c4d1de
Luca <james.lol2000@gmail.com>
2024-07-04 09:23:03 +0200

    Fix format

--- lang/src/parser.rs
+++ lang/src/parser.rs
@@ -50,161 +50,165 @@ impl TryInto<Module> for Parser {
     }
 }
 
-    fn parse_expression(&mut self) -> ParseResult<Expr> {
-// -------------------------------------------------------------------------------------------
-// Parse Expression
-impl Parser {
-    pub fn parse_expression(&mut self) -> ParseResult<Expr> {
-        let first = self.parse_primary_expression()?;
-        self.parse_binary_expression(first, 0)
-    }
-
-    fn parse_primary_expression(&mut self) -> ParseResult<Expr> {
-        let Token { span, kind } = self.peek()?;
-
-        match kind {
-            TokenKind::Identifier(Identifier::UserDefined(name)) => {
-                self.input.advance();
-                self.parse_expression_identifier(&name)
-            }
-            TokenKind::Literal(literal) => {
-                self.input.advance();
-                Ok(Expr::Literal(literal.clone()))
-            }
-            TokenKind::Identifier(Identifier::LParen) => {
-                self.input.advance();
-                let expr = self.parse_expression()?;
-                self.consume_checked(TokenKind::Identifier(Identifier::RParen))?;
-                Ok(expr)
-            }
-            _ => Err(Error::new(
-                span,
-                ErrorKind::UnexpectedToken {
-                    found: kind,
-                    expected: None,
-                },
-            )),
+fn parse_expression(&mut self) -> ParseResult<Expr> {
+    // -------------------------------------------------------------------------------------------
+    // Parse Expression
+    impl Parser {
+        pub fn parse_expression(&mut self) -> ParseResult<Expr> {
+            let first = self.parse_primary_expression()?;
+            self.parse_binary_expression(first, 0)
         }
-    }
 
-    /// This parses everything that starts with an identifier. Variables, function calls, etc.
-    fn parse_expression_identifier(&mut self, identifier: &str) -> ParseResult<Expr> {
-        if let Ok(TokenKind::Identifier(Identifier::LParen)) = self.peek().map(|t| t.kind) {
-            self.input.advance();
-            let mut args = Vec::new();
-            loop {
-                if let Ok(input) = self.parse_expression() {
-                    args.push(input);
-                }
+        fn parse_primary_expression(&mut self) -> ParseResult<Expr> {
+            let Token { span, kind } = self.peek()?;
 
-                if self.is_next_token(TokenKind::Identifier(Identifier::Comma)) {
+            match kind {
+                TokenKind::Identifier(Identifier::UserDefined(name)) => {
                     self.input.advance();
-                } else {
-                    break;
+                    self.parse_expression_identifier(&name)
                 }
+                TokenKind::Literal(literal) => {
+                    self.input.advance();
+                    Ok(Expr::Literal(literal.clone()))
+                }
+                TokenKind::Identifier(Identifier::LParen) => {
+                    self.input.advance();
+                    let expr = self.parse_expression()?;
+                    self.consume_checked(TokenKind::Identifier(Identifier::RParen))?;
+                    Ok(expr)
+                }
+                _ => Err(Error::new(
+                    span,
+                    ErrorKind::UnexpectedToken {
+                        found: kind,
+                        expected: None,
+                    },
+                )),
             }
-            self.expect_token(TokenKind::Identifier(Identifier::RParen))?;
-            self.input.advance();
-            Ok(Expr::FunctionCall(identifier.to_string()))
-        } else {
-            Ok(Expr::Variable(identifier.to_string()))
         }
-    }
 
-    fn current_precedence(&mut self) -> i16 {
-        self.peek()
-            .map(|t| BinaryOperator::try_from(t).map(|op| op.precedence()))
-            .unwrap_or(Ok(-1))
-            .unwrap_or(-1)
-    }
+        /// This parses everything that starts with an identifier. Variables, function calls, etc.
+        fn parse_expression_identifier(&mut self, identifier: &str) -> ParseResult<Expr> {
+            if let Ok(TokenKind::Identifier(Identifier::LParen)) = self.peek().map(|t| t.kind) {
+                self.input.advance();
+                let mut args = Vec::new();
+                loop {
+                    if let Ok(input) = self.parse_expression() {
+                        args.push(input);
+                    }
 
-    fn parse_binary_expression(&mut self, mut lhs: Expr, precendence: i16) -> ParseResult<Expr> {
-        while let Ok(token) = self.peek() {
-            let op = match BinaryOperator::try_from(token) {
-                Ok(op) => op,
-                Err(_) => {
+                    if self.is_next_token(TokenKind::Identifier(Identifier::Comma)) {
+                        self.input.advance();
+                    } else {
+                        break;
+                    }
+                }
+                self.expect_token(TokenKind::Identifier(Identifier::RParen))?;
+                self.input.advance();
+                Ok(Expr::FunctionCall(identifier.to_string()))
+            } else {
+                Ok(Expr::Variable(identifier.to_string()))
+            }
+        }
+
+        fn current_precedence(&mut self) -> i16 {
+            self.peek()
+                .map(|t| BinaryOperator::try_from(t).map(|op| op.precedence()))
+                .unwrap_or(Ok(-1))
+                .unwrap_or(-1)
+        }
+
+        fn parse_binary_expression(
+            &mut self,
+            mut lhs: Expr,
+            precendence: i16,
+        ) -> ParseResult<Expr> {
+            while let Ok(token) = self.peek() {
+                let op = match BinaryOperator::try_from(token) {
+                    Ok(op) => op,
+                    Err(_) => {
+                        return Ok(lhs);
+                    }
+                };
+                if op.precedence() < precendence {
                     return Ok(lhs);
                 }
-            };
-            if op.precedence() < precendence {
-                return Ok(lhs);
-            }
 
-            self.input.advance();
-
-            let mut rhs = self.parse_primary_expression()?;
-
-            if op.precedence() < self.current_precedence() {
-                rhs = self.parse_binary_expression(rhs, op.precedence() + 1)?;
-            }
-
-            lhs = Expr::Binary(BinaryExpression::new(lhs.clone(), op, rhs));
-        }
-        Ok(lhs)
-    }
-}
-
-// -------------------------------------------------------------------------------------------
-// Simple Parsers
-
-impl Parser {
-    fn parse_user_defined_identifier(&mut self) -> ParseResult<String> {
-        match self.peek()? {
-            Token {
-                kind: TokenKind::Identifier(Identifier::UserDefined(name)),
-                ..
-            } => {
                 self.input.advance();
-                Ok(name)
+
+                let mut rhs = self.parse_primary_expression()?;
+
+                if op.precedence() < self.current_precedence() {
+                    rhs = self.parse_binary_expression(rhs, op.precedence() + 1)?;
+                }
+
+                lhs = Expr::Binary(BinaryExpression::new(lhs.clone(), op, rhs));
             }
-            tok => Err(Error::unexpected_token(tok.span, tok.kind, None)),
+            Ok(lhs)
         }
     }
 
-    fn parse_literal(&mut self) -> ParseResult<Literal> {
-        match self.peek()? {
-            Token {
-                kind: TokenKind::Literal(literal),
-                ..
-            } => {
+    // -------------------------------------------------------------------------------------------
+    // Simple Parsers
+
+    impl Parser {
+        fn parse_user_defined_identifier(&mut self) -> ParseResult<String> {
+            match self.peek()? {
+                Token {
+                    kind: TokenKind::Identifier(Identifier::UserDefined(name)),
+                    ..
+                } => {
+                    self.input.advance();
+                    Ok(name)
+                }
+                tok => Err(Error::unexpected_token(tok.span, tok.kind, None)),
+            }
+        }
+
+        fn parse_literal(&mut self) -> ParseResult<Literal> {
+            match self.peek()? {
+                Token {
+                    kind: TokenKind::Literal(literal),
+                    ..
+                } => {
+                    self.input.advance();
+                    Ok(literal)
+                }
+                tok => Err(Error::unexpected_token(tok.span, tok.kind, None)),
+            }
+        }
+    }
+    // Parser helpers
+    impl Parser {
+        fn is_next_token(&mut self, expected: TokenKind) -> bool {
+            self.peek().map_or(false, |t| t.kind == expected)
+        }
+
+        fn expect_token(&mut self, expected: TokenKind) -> ParseResult<()> {
+            let Token { kind, span } = self.peek()?;
+
+            if kind == expected {
+                Ok(())
+            } else {
+                Err(Error::unexpected_token(span, kind, Some(expected)))
+            }
+        }
+
+        fn consume_checked(&mut self, expected: TokenKind) -> ParseResult<()> {
+            let Token { kind, span } = self.peek()?;
+
+            if kind == expected {
                 self.input.advance();
-                Ok(literal)
+                Ok(())
+            } else {
+                Err(Error::unexpected_token(span, kind, Some(expected)))
             }
-            tok => Err(Error::unexpected_token(tok.span, tok.kind, None)),
         }
-    }
 
-}
-// Parser helpers
-impl Parser {
-    fn is_next_token(&mut self, expected: TokenKind) -> bool {
-        self.peek().map_or(false, |t| t.kind == expected)
-    }
-
-    fn expect_token(&mut self, expected: TokenKind) -> ParseResult<()> {
-        let Token { kind, span } = self.peek()?;
-
-        if kind == expected {
-            Ok(())
-        } else {
-            Err(Error::unexpected_token(span, kind, Some(expected)))
+        fn peek(&mut self) -> ParseResult<Token> {
+            self.input
+                .peek()
+                .ok_or(Error::new(Span::default(), ErrorKind::UnexpectedEOF))
         }
-    }
-
-    fn consume_checked(&mut self, expected: TokenKind) -> ParseResult<()> {
-        let Token { kind, span } = self.peek()?;
-
-        if kind == expected {
-            self.input.advance();
-            Ok(())
-        } else {
-            Err(Error::unexpected_token(span, kind, Some(expected)))
-        }
-    }
-
-    fn peek(&mut self) -> ParseResult<Token> {
-        self.input
-            .peek()
-            .ok_or(Error::new(Span::default(), ErrorKind::UnexpectedEOF))
     }
 }
